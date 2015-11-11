098b48f39cccc70ae35f10cdcccba41ed670db81
lsj <lee2man@naver.com>
2015-11-11 10:18:47 +0900

    TeamProject

---
+++ src/main/java/com/cafe24/specialpos/commons/PageHelper.java
@@ -0,0 +1,5 @@
+package com.cafe24.specialpos.commons;
+
+public class PageHelper {
+
+}
---
+++ src/main/java/com/cafe24/specialpos/model/SposMall.java
@@ -0,0 +1,97 @@
+package com.cafe24.specialpos.model;
+
+public class SposMall {
+	private String mallCode;
+	private String mallAddr;
+	private String mallName;
+	private String mallPhone;
+	private String mallCeoName;
+	private String mallCeoPhone;
+	private String mallBizCode;
+	private String mallBizType;
+	private String mallBizStat;
+	private String mallBizClass;
+	private String mallClass;
+	private String mallBelong;
+	private int mallInvMoney;
+	
+	public String getMallCode() {
+		return mallCode;
+	}
+	public void setMallCode(String mallCode) {
+		this.mallCode = mallCode;
+	}
+	public String getMallAddr() {
+		return mallAddr;
+	}
+	public void setMallAddr(String mallAddr) {
+		this.mallAddr = mallAddr;
+	}
+	public String getMallName() {
+		return mallName;
+	}
+	public void setMallName(String mallName) {
+		this.mallName = mallName;
+	}
+	public String getMallPhone() {
+		return mallPhone;
+	}
+	public void setMallPhone(String mallPhone) {
+		this.mallPhone = mallPhone;
+	}
+	public String getMallCeoName() {
+		return mallCeoName;
+	}
+	public void setMallCeoName(String mallCeoName) {
+		this.mallCeoName = mallCeoName;
+	}
+	public String getMallCeoPhone() {
+		return mallCeoPhone;
+	}
+	public void setMallCeoPhone(String mallCeoPhone) {
+		this.mallCeoPhone = mallCeoPhone;
+	}
+	public String getMallBizCode() {
+		return mallBizCode;
+	}
+	public void setMallBizCode(String mallBizCode) {
+		this.mallBizCode = mallBizCode;
+	}
+	public String getMallBizType() {
+		return mallBizType;
+	}
+	public void setMallBizType(String mallBizType) {
+		this.mallBizType = mallBizType;
+	}
+	public String getMallBizStat() {
+		return mallBizStat;
+	}
+	public void setMallBizStat(String mallBizStat) {
+		this.mallBizStat = mallBizStat;
+	}
+	public String getMallBizClass() {
+		return mallBizClass;
+	}
+	public void setMallBizClass(String mallBizClass) {
+		this.mallBizClass = mallBizClass;
+	}
+	public String getMallClass() {
+		return mallClass;
+	}
+	public void setMallClass(String mallClass) {
+		this.mallClass = mallClass;
+	}
+	public String getMallBelong() {
+		return mallBelong;
+	}
+	public void setMallBelong(String mallBelong) {
+		this.mallBelong = mallBelong;
+	}
+	public int getMallInvMoney() {
+		return mallInvMoney;
+	}
+	public void setMallInvMoney(int mallInvMoney) {
+		this.mallInvMoney = mallInvMoney;
+	}
+	
+}
