52d4a1b6a6c9a135f4c58faeaaf0dee61d1f64d2
lsj <lee2man@naver.com>
2015-11-18 10:06:59 +0900

    loss & mall

--- src/main/java/com/specialpos/loss/service/SposLossDao.java
+++ src/main/java/com/specialpos/loss/service/SposLossDao.java
@@ -1,5 +1,13 @@
 package com.specialpos.loss.service;
 
-public interface SposLossDao {
+import java.util.List;
 
+import com.specialpos.loss.domain.SposLoss;
+
+public interface SposLossDao {
+	public int insertLoss(SposLoss sposLoss);
+	public SposLoss getLossForModify(String LossCode);
+	public int modifyLoss(SposLoss sposLoss);
+	public int delteLoss(SposLoss sposLoss);
+	public List<SposLoss>getLossList(String serachWord);
 }
--- src/main/java/com/specialpos/loss/service/SposLossService.java
+++ src/main/java/com/specialpos/loss/service/SposLossService.java
@@ -1,5 +1,13 @@
 package com.specialpos.loss.service;
 
-public interface SposLossService {
+import java.util.List;
 
+import com.specialpos.loss.domain.SposLoss;
+
+public interface SposLossService {
+	public int insertLoss(SposLoss sposLoss);
+	public SposLoss getLossForModify(String lossCode);
+	public int modifyLoss(String lossCode);
+	public int deleteLoss(String lossCode);
+	public List<SposLoss> getLossList(String searchWord);
 }
